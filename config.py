1f7b3cbd0528b70c14f032bea39353d06d0f0c4a
Vladislav Prozorov <prozorov93@gmail.com>
2021-12-29 22:10:20 +0500

    Del token string

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 """
 config.py содержит переменную token, которая содержит токен телеграм бота созданный через @BotFather
 """
-token = '5027293365:AAG-6ByGmG7WqSnEX23xPFr9PEuxJ_iiBK8'
+token = ''
 
